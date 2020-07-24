6b7882a804258096862322e2c3d539ecb1934c92
Dat Vu <dat.vu@nng.bz>
2020-07-24 15:21:29 +0700

    next

--- src/reducers/userProfile.js
+++ src/reducers/userProfile.js
@@ -6,6 +6,9 @@ const userProfile = (state = initialState, action) => {
     switch (action.type) {
         case types.GET_USER_PROFILE:
             return {...state, ...action.payload};
+        case types.LOG_OUT:
+            state = {};
+            return state;
         default:
             return state;
     }
